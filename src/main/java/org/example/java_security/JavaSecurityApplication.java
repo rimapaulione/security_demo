ebd87fab7a044abfa4dede3b7468fe328e601f83
Rima Paulionė <rimapaulione@gmail.com>
2026-03-22 11:12:45 +0200

    first commit

---
+++ src/main/java/org/example/java_security/JavaSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.java_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/java_security/JavaSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
